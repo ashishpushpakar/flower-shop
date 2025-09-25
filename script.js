fc7e535d260ef497a616bf9bdc030ac84d1338ad
ashish pushpakar <ashishpushpakar7037@gmail.com>
2025-09-25 20:45:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+<div class="product-card" data-name="haldi cerimony" data-price="2k-5k">
+  <img src="https://via.placeholder.com/250x200?text=Roses" alt="haldi">
+  <h3>haldi cerimony</h3>
+  <p>2k-5k</p>
+  <button class="add-to-cart">Add to Cart</button>
+</div>
+
+<div class="product-card" data-name="car decoration" data-price="3k-7k>
+  <img src="https://via.placeholder.com/250x200?text=car decoration" alt="cars">
+  <h3>car decoration</h3>
+  <p>3k-7k</p>
+  <button class="add-to-cart">Add to Cart</button>
+</div>
+
+<div class="product-card" data-name="bed room decoration" data-price="4k-10k">
+  <img src="https://via.placeholder.com/250x200?text=Sunflowers" alt="bedrooms">
+  <h3>bedroom decoration</h3>
+  <p>4k-10k</p>
+  <button class="add-to-cart">Add to Cart</button>
+</div>
+
+<div class="product-card" data-name="stage decorations" data-price="5k-15k">
+  <img src="https://via.placeholder.com/250x200?text=Lilies" alt="stages">
+  <h3>stages decorations</h3>
+  <p>5k-15k</p>
+  <button class="add-to-cart">Add to Cart</button>
+</div>
